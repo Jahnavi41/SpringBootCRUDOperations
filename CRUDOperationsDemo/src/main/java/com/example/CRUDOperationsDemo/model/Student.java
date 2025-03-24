44918fcd1623eefc44f6968ad6f436e790bf5121
Jahnavi41 <astrantia236@gmail.com>
2025-03-24 12:03:37 +0530

    Added Entity to Student Model

--- CRUDOperationsDemo/src/main/java/com/example/CRUDOperationsDemo/model/Student.java
+++ CRUDOperationsDemo/src/main/java/com/example/CRUDOperationsDemo/model/Student.java
@@ -1,15 +1,22 @@
 package com.example.CRUDOperationsDemo.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 import java.time.LocalDate;
 
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Student {
 
-    private String studentName;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String studentUSN;
+    private String studentName;
     private int studentAge;
     private int studentSem;
     private String studentClass;
